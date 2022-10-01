328a9cde73a297c1cc4e2d63e3ee485e1422cdaf
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2022-09-30 23:06:28 -0300

    first commit

---
+++ src/data/news.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: 'Upstairs Downstairs',
+        body: 'Now then, survivors. Let’s do the thing. NEXT This week we released version 41.77 into our Unstable beta. This…',
+        author: 'News & Dev',
+        date: 'September 29, 2022',
+        img: {
+            src: 'https://projectzomboid.com/blog/content/uploads/2022/09/wowssers.png',
+            alt: 'train image'
+        },
+    },
+    {
+        title: 'The Good Life',
+        body: 'Hello everyone. NEXT The situation with Linux server instability remains ongoing, with the MP team working on another update to…',
+        author: 'News & Dev',
+        date: 'September 15, 2022',
+        img: {
+            src: 'https://projectzomboid.com/blog/content/uploads/2022/09/kawri.png',
+            alt: 'reading image'
+        },
+    },
+    {
+        title: 'Broadened HorizonZ',
+        body: 'Okay, so here’s a run down on our current basic plans for finalizing Build 41, and then beyond. NEXT We’ve…',
+        author: 'News & Dev',
+        date: 'September 1, 2022',
+        img: {
+            src: 'https://projectzomboid.com/blog/content/uploads/2022/09/universe-of-gamer-use.png',
+            alt: 'reading image'
+        },
+    }
+]
